57eb0c710a9b54465a4191f3e8806ebdd4aea46f
Samantha Yengst <Samantha.Yengst@gmail.com>
2026-08-16 13:13:30 -0500

    El Nino Data Import

--- test.py
+++ test.py
@@ -3,41 +3,42 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
-# 
-url = "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_mlo.csv"
+# Importing data for Co2 levels
+noaa_co2_url = "https://gml.noaa.gov/webdata/ccgg/trends/co2/co2_mm_mlo.csv"
 
-response = requests.get(url)
-print(response.status_code)
-# print(response.text[:500])
+co2_res = requests.get(noaa_co2_url)
+print(co2_res.status_code)
+# print(co2_res.text[:500])
+
+# Importing data for El Nino 
+noaa_el_nino_url = "https://www.cpc.ncep.noaa.gov/data/indices/oni.ascii.txt"
+
+nino_res = requests.get(noaa_co2_url)
+print(nino_res.status_code)
+print(nino_res.text[:500])
 
 
-frame = pd.read_csv(StringIO(response.text), comment="#")
-# print(frame.head())
+# frame = pd.read_csv(StringIO(co2_res.text), comment="#")
 
 
-# print(f"Date range: {frame['year'].min()} to {frame['year'].max()}")
-# print(f"CO2 in 1958: {frame[frame['year'] == 1958]['average'].mean():.2f} ppm")
-# print(f"CO2 in 2024: {frame[frame['year'] == 2024]['average'].mean():.2f} ppm")
-# print(f"Total increase: {frame[frame['year'] == 2024]['average'].mean() - frame[frame['year'] == 1958]['average'].mean():.2f} ppm")
+# # Getting only data from 2007 + 
+# myLifetime = frame[frame['year'].astype(int) >= 2007]
+# print(myLifetime.describe())
 
-# Getting only data from 2007 + 
-myLifetime = frame[frame['year'].astype(int) >= 2007]
-print(myLifetime.describe())
+# # Average Co2 level every year of my lifetime
+# myLifetimeyoy = myLifetime.groupby('year')['average'].mean()
+# print(f"{myLifetimeyoy}")
 
-# Average Co2 level every year of my lifetime
-myLifetimeyoy = myLifetime.groupby('year')['average'].mean()
-print(f"{myLifetimeyoy}")
+# # Inc every year from 2007 to present
+# print(f"Yearly difference:\n{myLifetimeyoy.diff()}")
 
-# Inc every year from 2007 to present
-print(f"Yearly difference:\n{myLifetimeyoy.diff()}")
+# fig, ax1 = plt.subplots()
 
-fig, ax1 = plt.subplots()
+# ax1.set_xticks(myLifetimeyoy.index[::2])
 
-ax1.set_xticks(myLifetimeyoy.index[::2])
+# ax2 = ax1.twinx()
 
-ax2 = ax1.twinx()
+# myLifetimeyoy.plot(ax=ax1, color='blue')
+# myLifetimeyoy.diff().plot(ax=ax2, color='red')
 
-myLifetimeyoy.plot(ax=ax1, color='blue')
-myLifetimeyoy.diff().plot(ax=ax2, color='red')
-
-plt.show()
+# plt.show()
